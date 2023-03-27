449dd9df0779561d33c5e91b1b48eb477179f09e
merarimaysosa <merari_merry12@hotmail.com>
2023-03-26 21:59:30 -0600

    Clase suma, adiciones de texto

--- src/main/java/org/example/Suma.java
+++ src/main/java/org/example/Suma.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 public class Suma {
    static int a=3, b=2, suma=0;
 
     public static void main(String[] args) {
         System.out.println(suma = a + b);
+        System.out.println("Suma efectuada!!");
     }
 }
